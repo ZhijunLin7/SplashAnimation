50ad3f1c941fb41b77ba34ff2b16d473678b5dda
zhijunlin7 <zlin1@cifpfbmoll.eu>
2022-12-19 19:44:43 +0100

    animacion

--- app/src/main/java/com/example/splashanimation/SplashAnimation.java
+++ app/src/main/java/com/example/splashanimation/SplashAnimation.java
@@ -2,6 +2,7 @@ package com.example.splashanimation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -17,7 +18,56 @@ public class SplashAnimation extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim);
-        binding.titol.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
 
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Animation animation2 = AnimationUtils.loadAnimation(SplashAnimation.this, R.anim.anim2);
+                animation2.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        Intent intent=new Intent(SplashAnimation.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+                binding.titol2.startAnimation(animation2);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        binding.titol.startAnimation(animation);
+        binding.imageview1.startAnimation(animation);
+        binding.imageview2.startAnimation(animation);
+        binding.imageview3.startAnimation(animation);
+        binding.imageview4.startAnimation(animation);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        binding.titol.clearAnimation();
+        binding.imageview1.clearAnimation();
+        binding.imageview2.clearAnimation();
+        binding.imageview3.clearAnimation();
+        binding.imageview4.clearAnimation();
+        binding.titol2.clearAnimation();
     }
 }
